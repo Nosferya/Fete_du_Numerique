25f89f9869f375f574b8d6ad29ca8a24a3a85a07
aportier2018 <anne.portier@wanadoo.fr>
2018-07-16 17:10:38 +0200

    un peu de nettoyage

--- accueil.php
+++ accueil.php
@@ -34,9 +34,6 @@
 				dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
 
-		<!-- <div class="presentation"> -->
-
-	<!-- </div> -->
 	<img src="images/ombre_place.png" alt="" class="ombreplace">
 	</section>
 	<!-- FIN PARTIE PRESENTATION -->
